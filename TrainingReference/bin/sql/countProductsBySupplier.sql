ba46c27130e91771d81576f21ff48c3797f1f86c
sridharreddy597 <sridhar.ramsasahayam@gmail.com>
2019-09-14 10:25:21 -0400

    deleted unwanted folders

--- TrainingReference/bin/sql/countProductsBySupplier.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE CountProductsBySupplier;
-
-CREATE PROCEDURE CountProductsBySupplier(
-         IN suppId INT,
-         OUT total INT)
-BEGIN
-    SELECT count(*) INTO total FROM products WHERE supplierId = suppId;
-END
